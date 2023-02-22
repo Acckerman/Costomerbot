e1d2869327bf4144f3ee43bd63cfd3ac3e552f9b
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-02-22 18:41:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://telegram.me/k2m_movies_series')] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://telegram.me/Malyalam_Mallu_Movies')] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
