89256a9773d78bb60013baa3a793116e4098dc1f
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-02-25 08:39:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -377,11 +377,6 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-async def get_shortlink(link):
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
 
     
             
